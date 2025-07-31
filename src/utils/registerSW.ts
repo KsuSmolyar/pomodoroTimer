4b837a68af59f6fe2e14d124fc2752a485de05b5
ksu_smolyar <ksu_smolyar@mail.ru>
2025-07-31 12:57:23 +0400

    fix

--- src/utils/registerSW.ts
+++ src/utils/registerSW.ts
@@ -2,7 +2,7 @@ export function registerSW() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register(`${import.meta.env.BASE_URL}sw.js`)
+        .register(`/pomodoroTimer/sw.js`)
         .then(reg => console.log('SW registered', reg))
         .catch(err => console.log('SW registration failed:', err));
     });
